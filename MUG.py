6698c6cfd1cbffd82651abd7c6c20bb851175e5b
Anuran Pal <anuranpal98@gmail.com>
2021-11-12 10:32:08 +0530

    MUG base

--- MUG.py
+++ MUG.py
@@ -18,26 +18,32 @@ def strategy_update(payoff_arr, strategy_arr, Z, beta, mu, eps=0.05):
     prob = 1./(1+np.exp((payoff1-payoff2)*beta))
     
     if np.random.random()<prob:
-        strategy_arr[plCentral,:] = strategy_arr[pl[1],:] + eps*(np.random.random()-0.5)
+        strategy_arr[plCentral,:] = strategy_arr[pl[1],:] + eps*(
+            np.random.random()-0.5)
         
     return strategy_arr
 
 
+# Playing a single MUG in a group of N selected from Z individuals with a minimum of M(<N) required for the proposal to pass.
 @nb.njit
 def single_MUG(strategy_arr, N, M, Z):
 
     payoff_arr = np.zeros((Z,))
     acceptance_arr = np.zeros((N,1))
+
+    # Select N from the Z players
     selP = np.random.choice(Z, N, replace=False)
     RespP = np.zeros((N-1,))
+
+    # Cycle through the proposals of each player in the group
     index = -1
     for PropP in selP:
         index = index + 1
-        RespP = np.delete(selP, index)
+        RespP = np.delete(selP, index) #Responders
         p = strategy_arr[PropP,0]
-        acceptInd = p>=strategy_arr[RespP,1]
+        acceptInd = p>=strategy_arr[RespP,1] #array of True False
         acceptancerate = np.sum(acceptInd)
-        if acceptancerate>=M:
+        if acceptancerate>=M: #M is required for group acceptance
             payoff_arr[PropP] = payoff_arr[PropP] + (1-p)
             payoff_arr[RespP] = payoff_arr[RespP] + p/(N-1)
         else:
@@ -48,6 +54,15 @@ def single_MUG(strategy_arr, N, M, Z):
 if __name__ == "__main__":
 
     import time
+    tt = time.time()
+
+    '''
+    np.random.seed(1)
+    # MUG check
+    strategies = np.array([[0.5,0.5],[0.6,0.1],[0.2,0.1],[0.4,0.2]])
+    payoff = single_MUG(strategies, 3, 2, 4)
+    print(payoff)    
+    '''
     
     Z = 1024
     gen = 4000
@@ -55,10 +70,8 @@ if __name__ == "__main__":
     N = 9
     M = 7
     beta = 10
-
-    #strategies = np.array([[0.5,0.5],[0.6,0.1],[0.2,0.1],[0.4,0.2]])
     strategies = np.random.random((Z,2))
-    tt = time.time()
+    
     for g in range(gen):
         payoffs = np.zeros((Z,))
         for i in range(nMUG):
@@ -69,3 +82,4 @@ if __name__ == "__main__":
         print(np.mean(strategies, axis=0))
     print(time.time()-tt)
     print(payoffs)
+    
